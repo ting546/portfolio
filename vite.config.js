36808c5804d61cc7fd616b8221ec0ca01fadfca2
ting546 <igorbeldanov62@gmail.com>
2023-10-14 13:53:59 +0600

    add project

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,20 @@ import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "url";
 import vue from "@vitejs/plugin-vue";
 import vitePluginRequire from "vite-plugin-require";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   base: "/portfolio",
-  plugins: [vue(), vitePluginRequire.default()],
+  plugins: [
+    vue(),
+    vitePluginRequire.default(),
+    viteStaticCopy({
+      targets: [
+        // Копирование содержимого папки src в папку dist
+        { src: "src/project", dest: "." },
+      ],
+    }),
+  ],
   resolve: {
     alias: [
       { find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)) },
