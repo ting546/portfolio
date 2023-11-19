e63489398892ac21751ca2181eb5fe7f233aabb8
ting546 <igorbeldanov62@gmail.com>
2023-11-19 18:57:32 +0600

    bug fixes

--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import { createApp } from "vue";
 import "./style.scss";
 import App from "./App.vue";
 
+
 const app = createApp(App);
-app.mount("#app");
+
+if (false){
+  app.mount("#app");
+}
+
+
+
